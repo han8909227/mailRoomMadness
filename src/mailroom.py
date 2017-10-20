9cf577550d6075620acea39231daa6761b84ed2f
eldocwho <fortunato.maycotte@gmail.com>
2017-10-20 15:49:36 -0700

    Test for Avg We tested the avarage of donations

---
+++ src/mailroom.py
@@ -0,0 +1,14 @@
+""" . """
+
+def user_report(donor_dictionary):
+    pass
+
+def avg_donation(donations):
+    if not donations:
+        return 0
+    return sum(donations)/len(donations)
+
+
+
+
+    
